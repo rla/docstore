a8d7fddec32d3d66132f7c1dda529378497e7c8c
Raivo Laanemets <raivo@infdot.com>
2014-04-22 12:46:45 +0300

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('docstore').
-version('1.0.0').
+version('1.0.1').
 title('Document-oriented database for Prolog').
 author('Raivo Laanemets', 'http://rlaanemets.com/').
 home('https://github.com/rla/docstore').
